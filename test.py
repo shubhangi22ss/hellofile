d1ca54dfae174e2e9790c75d5dfca6dfe5cb6b65
shubhangi22ss <78610563+shubhangi22ss@users.noreply.github.com>
2022-04-29 10:54:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
+# A school method based Python3
+# program to check if a number
+# is prime
+n=11
+def isPrime(n):
+
+	# Corner case
+	if n <= 1:
+		return False
+
+	# Check from 2 to n-1
+	for i in range(2, n):
+		if n % i == 0:
+			return False;
+
+	return True
+
+# Driver Program to test above function
 
 
-num = 11
-
-# If given number is greater than 1
-if num > 1:
-
-	# Iterate from 2 to n / 2
-	for i in range(2, int(num/2)+1):
-
-		# If num is divisible by any number between
-		# 2 and n / 2, it is not prime
-		if (num % i) == 0:
-			print(num, "is not a prime number")
-			break
-	else:
-		print(num, "is a prime number")
-
-else:
-	print(num, "is not a prime number")
